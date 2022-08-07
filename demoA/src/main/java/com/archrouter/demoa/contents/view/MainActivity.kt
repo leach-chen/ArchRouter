c4bfb2a798c36ad34296a6e6a76e6c25c756cf3a
leach-chen <349763031@qq.com>
2022-08-07 12:11:54 +0800

    [plugin]添加目录lib&添加plugin控制application&lib切换功能

---
+++ demoA/src/main/java/com/archrouter/demoa/contents/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.archrouter.demoa.contents.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.archrouter.demoa.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ demoB/src/main/java/com/archrouter/demob/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.archrouter.demob.contents.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.archrouter.demob.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
