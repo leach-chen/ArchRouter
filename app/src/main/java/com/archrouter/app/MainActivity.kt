42a26efda9af9bf6b6b185116cec042e3b3ec44c
leach-chen <349763031@qq.com>
2022-11-04 18:45:36 +0800

    添加页面跳转数据传递回传用法

--- app/src/main/java/com/archrouter/app/MainActivity.kt
+++ app/src/main/java/com/archrouter/app/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.archrouter.app
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.archrouter.arch.annotation.Print
 import com.archrouter.provider.constants.ConstantsUri
 import com.archrouter.provider.interfaces.demoA.ITestDemoA
 import com.archrouter.router.Router
+import com.archrouter.router.common.DefaultUriRequest
+import com.archrouter.router.components.UriSourceTools
+import com.archrouter.router.core.OnCompleteListener
+import com.archrouter.router.core.UriRequest
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,5 +42,28 @@ class MainActivity : AppCompatActivity() {
 
     fun goFun3(view: View) {
         Router.startUri(this, ConstantsUri.PATH_DEMOB)
+    }
+
+    fun goFun4(view: View) {
+        //Router.startUri(this, ConstantsUri.PATH_DEMOB)
+        DefaultUriRequest(this, ConstantsUri.PATH_DEMOB) //传入context和目标uri
+            // startActivityForResult使用的RequestCode
+            .activityRequestCode(100) // 设置跳转来源，默认为内部跳转，还可以是来自WebView、来自Push通知等。
+            // 目标Activity可通过UriSourceTools区分跳转来源。
+            .from(UriSourceTools.FROM_INTERNAL) // Intent加参数
+            .putExtra("testValue", "来自主工程的数据")
+            .onComplete(object : OnCompleteListener {
+                override fun onSuccess(request: UriRequest) {
+                    //ToastUtils.showToast(request.context, "跳转成功")
+                }
+
+                override fun onError(request: UriRequest, resultCode: Int) {}
+            }) // 这里的start实际也是调用了Router.startUri方法
+            .start()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Toast.makeText(this, data?.getStringExtra("testValue"), Toast.LENGTH_SHORT).show()
     }
 }
--- demoB/src/main/java/com/archrouter/demob/view/DemoBActivity.kt
+++ demoB/src/main/java/com/archrouter/demob/view/DemoBActivity.kt
@@ -1,15 +1,28 @@
 package com.archrouter.demob.contents.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.archrouter.arch.annotation.RouterUri
 import com.archrouter.demob.R
 import com.archrouter.provider.constants.ConstantsUri
+import com.archrouter.router.Router
 
 @RouterUri(path = [ConstantsUri.PATH_DEMOB])
 class DemoBActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demob)
+
+        if (this.intent?.extras?.getString("testValue") != null) {
+            Toast.makeText(this, this.intent?.extras?.getString("testValue"), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun goFun1(view: View) {
+        setResult(100, Intent().putExtra("testValue","DemoB的回传的数据"))
+        this.finish()
     }
 }
